4b435f0c601700d013edff7cd42ef2e787b9d999
Victor Averin <victor.p.averin@gmail.com>
2020-09-30 18:17:20 +0300

    Upgrade skeleton structure

--- Cash/shopping.js
+++ Cash/shopping.js
@@ -2,7 +2,8 @@ let button = document.querySelector ('button');
 button.onclick = function sum() {
 	
 	let setPrice = prompt ( 'Введіть суму на яку здійснюється покупка' , '');
-		
+			console.log ('Сума покупки : ' + setPrice + ' грн.')
+	
 	const DISCOUNT = 1000; 
 			
 		if (setPrice < DISCOUNT ) {
@@ -10,26 +11,28 @@ button.onclick = function sum() {
 			
 			} else if ( (setPrice > DISCOUNT) || (setPrice = DISCOUNT)  ) {
 				let randDiscount = Math.ceil( Math.random() * 15 );
-					console.log( randDiscount + '% - Ваша знижка ');  					
+					console.log('Знижка : ' + randDiscount + ' %.');  					
 
 				let getPrice = setPrice / 100 * randDiscount; 
 				
 				let price = setPrice - getPrice ;
 					alert ('Сума до сплати з урахуванням знижки ' + randDiscount + '% складає '  + price + ' грн.' ); 
-						
-				let delivery = confirm ('У разі придбання товара готівкою Ви матимете можливість скористатись безкоштовною доставкою ');
-					if (!delivery) {							
-						console.log ('Сума до сплати : ' + price + ' грн.')
-							
-					} else if (delivery){
+					console.log('Ціна товару з урахуванням знижки : ' + price + ' грн.');
+					console.log('Сума знижки : ' + getPrice + ' грн.');	
+				
+				let delivery = confirm ('У разі придбання товара готівкою Ви матимете можливість скористатись безкоштовною доставкою на відстань до 30 км. Бажаєте скористатись даною послугою?');
+					  if (delivery){
 						let distance = prompt ('Яка відстань (км) до місця доставки ?');
+							console.log('Відстань доставки : ' + distance + ' км.');
 							if ( distance > 30){
 								let deliveryPrice = (( distance - 30 ) * 5 );
 									alert ('Загальна сума до оплати за товар: ' + (price + deliveryPrice) + ' грн. Доплата за доставку товару становить : ' + deliveryPrice + ' грн.');
-							}else if ( distance = 30 || distance < 30) {
-								alert ('Загальна сума до оплати за товар : ' + price + ' грн. Ваш товар буде доставлений безкоштовно.' );
-								}
-							} 
-								
-		} 
-}
+									console.log('Доплата за доставку : ' + deliveryPrice + ' грн.');
+									console.log('Загальна сума до оплати за товар: ' + (price + deliveryPrice) + ' грн.');
+									
+							}else {							
+									console.log ('Загальна сума до оплати за товар : ' + price + ' грн. Ваш товар буде доставлений безкоштовно.')
+							}
+						} 						
+					} 
+				}
