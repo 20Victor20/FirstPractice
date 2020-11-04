a39eca87cb4c41ab7e01c8dcf5081ae23216a287
Victor Averin <victor.p.averin@gmail.com>
2020-11-04 17:43:09 +0200

    Add files for test HTML and JS opportunities

---
+++ CashRegisterWithForms/testJS.js
@@ -0,0 +1,14 @@
+function firstCount() {
+
+	let setPrice = document.getElementById("basePrice").value;
+	console.log ('Сума покупки : ' + setPrice + ' грн.')
+}
+
+function secondCount() {
+	
+	let element = document.getElementById('withDiscPrice');
+
+	console.log ( "Дякуюємо за викоритсання програми " + document.title + "!")
+	element.style.color = "blue"
+ 
+}
